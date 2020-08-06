cfa2f6834073e41c1857a7404f99971e351c19ee
AshishSharman <68974976+AshishSharman@users.noreply.github.com>
2020-08-05 22:19:33 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,9 @@
+import { yourLoginName } from '../../app/yourLoginName';
+
+export const myInfo: yourLoginName[] = [
+    {studentNumber: '991546656',
+    studentName: 'Ashish Sharma',
+    loginName: 'sharm629',
+    sheridanCampus: 'Davis',
+    assignmentTitle: 'Assignment 4'}
+];
